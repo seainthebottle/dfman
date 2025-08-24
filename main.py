73eae74ebb96bfa57e81d0172813ae33e95b105f
seainthebottle <seainthebottle@gmail.com>
2025-08-24 10:42:58 +0900

    설정을 일부 바꿈

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from src.main_window import MainWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = MainWindow()
-    win.resize(800, 600)
+    win.resize(1024, 768)
     win.show()
     sys.exit(app.exec())
